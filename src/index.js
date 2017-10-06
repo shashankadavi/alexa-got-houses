fe162f018c94d988a435b5515caea6644e7edc10
Pranay Shashank Adavi <shashank.ap28@gmail.com>
2017-10-06 00:15:29 -0400

    Added GoT houses to intent,slots and alexa sdk event handlers

--- src/index.js
+++ src/index.js
@@ -8,11 +8,42 @@ exports.handler = function(event, context, callback) {
 
 const handlers = {
     'LaunchRequest': function () {
-        this.emit('MyIntent');
+        var self = this; // use this to reference in request callback
+        this.attributes['houses'] = '';
+        let speechOutput = "Hi, Valar Morghalis, Which Game of Thrones House are you interested in?";
+        self.response.speak(speechOutput).listen(speechOutput);
+        self.emit(':responseReady');
     },
 
     'MyIntent': function () {
-        this.response.speak('This is a demo!');
+        let house = this.event.request.intent.slots['House'].value.toString().toLowerCase();
+        let outText = '';
+
+        if (house =='stark'){
+            this.response.speak('The words of House'+ house +'are: Winter Is Coming!');
+        }else if (house =='lannister') {
+            this.response.speak('The words of House'+ house +'are: A Lannister Always Pays His Debts');
+        }else if (house == 'targaryen') {
+            this.response.speak('The words of House'+ house +'are: Fire and Blood');
+        }else if (house == 'greyjoy') {
+            this.response.speak('The words of House'+ house +'are: What Is Dead May Never Die!');
+        }else if (house == 'frey') {
+            this.response.speak('The words of House'+ house +'are: We Stand Together');
+        }else if (house == 'arryn') {
+            this.response.speak('The words of House'+ house +'The words of House'+ house +'are: As High as Honor!');
+        }else if (house == 'tully') {
+            this.response.speak(house +'aint a great house anymore. Not worthy enough for Alexa, First of Her Name to say their words');
+        }else if (house != ''){
+            this.response.speak('Oh Noble Person! Only Great Game of Thrones Houses, Please!');
+        }
+        this.emit(':responseReady');
+    },
+
+    'SessionEndedRequest' : function() {
+
+    },
+    'Unhandled': function (){
+        this.response.speak('Apologies, I, Alexa, of House Echo The First Generation do not understand');
         this.emit(':responseReady')
     }
 };
